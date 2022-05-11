537ca9d8423034ae1e685b9934ecc0e371bc85d3
KosM <kosukemorimoto1@gmail.com>
2022-05-11 20:43:34 +0900

    added navigate functionality into directory component

--- src/components/directory-item/directory-item.component.tsx
+++ src/components/directory-item/directory-item.component.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import { type DirectoryCategory } from 'components/directory/directory.component';
+
 import './directory-item.styles.scss';
 
-const DirectoryItem = ({ category }: DirectoryItemProps) => {
-  const { imageUrl, title } = category;
+const DirectoryItem = ({
+  category: { title, imageUrl, route },
+}: DirectoryItemProps) => {
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={onNavigateHandler}>
       <div
         className="background-image"
         style={{
